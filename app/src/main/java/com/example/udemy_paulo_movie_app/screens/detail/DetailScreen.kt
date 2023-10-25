d1180ad71ec750b584a7b9c844cbd392873a9fb9
monopoly11141 <monopoly1114@vt.edu>
2023-10-25 11:27:27 +0900

    ~Lesson 118

--- app/src/main/java/com/example/udemy_paulo_movie_app/screens/detail/DetailScreen.kt
+++ app/src/main/java/com/example/udemy_paulo_movie_app/screens/detail/DetailScreen.kt
@@ -1,7 +1,10 @@
 package com.example.udemy_paulo_movie_app.screens.detail
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
@@ -9,11 +12,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
+import com.example.udemy_paulo_movie_app.model.Movie
+import com.example.udemy_paulo_movie_app.model.getMovies
+import com.example.udemy_paulo_movie_app.widgets.MovieItem
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DetailScreen(navController: NavController, movieData: String?) {
+
+fun DetailScreen(
+    navController: NavController,
+    movieId: String?
+) {
+
+    val movie = getMovies().first { it.id == movieId }
 
     Scaffold(
         topBar = {
@@ -43,7 +57,6 @@ fun DetailScreen(navController: NavController, movieData: String?) {
             )
 
 
-
         }
 
     }
@@ -51,13 +64,41 @@ fun DetailScreen(navController: NavController, movieData: String?) {
     Column(
         modifier = Modifier
             .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "$movieData",
-            style = MaterialTheme.typography.headlineMedium
+        MovieItem(movie = movie)
+
+        Spacer(
+            modifier = Modifier.height(8.dp)
         )
+
+        Divider()
+
+        Text("Movie Images")
+
+        HorizontalScrollableMovieImageView(movie)
     }
 
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun HorizontalScrollableMovieImageView(movie: Movie) {
+    LazyRow {
+        items(items = movie.images) { image ->
+            Card(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(240.dp),
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 5.dp
+                )
+            ) {
+                Image(
+                    painter = rememberAsyncImagePainter(model = image),
+                    contentDescription = "Movie Poster"
+                )
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/udemy_paulo_movie_app/widgets/widgets.kt
+++ app/src/main/java/com/example/udemy_paulo_movie_app/widgets/widgets.kt
@@ -1,35 +1,48 @@
 package com.example.udemy_paulo_movie_app.widgets
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.ImageLoader
 import coil.compose.rememberAsyncImagePainter
-import coil.transform.CircleCropTransformation
 import com.example.udemy_paulo_movie_app.model.Movie
+import com.example.udemy_paulo_movie_app.model.getMovies
 
 @OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
 @Composable
 fun MovieItem(
-    movie: Movie,
+    movie: Movie = getMovies()[0],
     onItemClicked: (String) -> Unit = {}
 ) {
+    var isExpandedStateValue by remember { mutableStateOf(false) }
+
     Card(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(125.dp)
             .clickable {
                 onItemClicked(movie.id)
             },
@@ -49,10 +62,6 @@ fun MovieItem(
                 shape = RectangleShape,
                 shadowElevation = 4.dp
             ) {
-//                Icon(
-//                    imageVector = Icons.Default.AccountBox,
-//                    contentDescription = "Movie Image"
-//                )
                 Image(
                     painter = rememberAsyncImagePainter(
                         model = movie.images[0],
@@ -74,16 +83,86 @@ fun MovieItem(
                     text = movie.title,
                     style = MaterialTheme.typography.headlineSmall
                 )
+
                 Text(
                     text = "Director ${movie.director}",
                     style = MaterialTheme.typography.bodySmall
                 )
+
                 Text(
                     text = "Released ${movie.year}",
                     style = MaterialTheme.typography.bodySmall
                 )
+
+                AnimatedVisibility(visible = isExpandedStateValue) {
+                    Column(
+
+                    ) {
+                        Text(
+                            buildAnnotatedString {
+                                withStyle(
+                                    style = SpanStyle(
+                                        color = Color.DarkGray,
+                                        fontSize = 14.sp
+                                    ),
+                                ) {
+                                    append("Plot : ")
+                                }
+
+                                withStyle(
+                                    style = SpanStyle(
+                                        color = Color.DarkGray,
+                                        fontSize = 14.sp,
+                                        fontWeight = FontWeight.Light
+                                    ),
+                                ) {
+                                    append(movie.plot)
+                                }
+                            },
+                            modifier = Modifier
+                                .padding(6.dp)
+                        )
+
+                        Divider(
+                            modifier = Modifier
+                                .padding(3.dp)
+                        )
+
+                        Text(
+                            text = "Director : ${movie.director}",
+                            style = MaterialTheme.typography.bodySmall
+                        )
+
+                        Text(
+                            text = "Actors : ${movie.actors}",
+                            style = MaterialTheme.typography.bodySmall
+                        )
+
+                        Text(
+                            text = "Rating : ${movie.rating}",
+                            style = MaterialTheme.typography.bodySmall
+                        )
+                    }
+                }
+
+                Icon(
+                    imageVector = if (isExpandedStateValue) {
+                        Icons.Filled.KeyboardArrowUp
+                    } else {
+                        Icons.Filled.KeyboardArrowDown
+                    },
+                    contentDescription = "Down Arrow",
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable {
+                            isExpandedStateValue = !isExpandedStateValue
+                        },
+                    tint = Color.DarkGray
+                )
             }
 
         }
+
+
     }
 }
